5ed78c3ae335b266065078be9b2218e418ba9ea5
johannes-hoetter <johannes.hoetter@smail.inf.h-brs.de>
2018-12-18 10:28:09 +0100

    Card Values get assigned

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,44 @@
+
+const cards = document.getElementsByClassName('card');
+
 function setup() {
-    const cards = document.getElementsByClassName('card')
+
+    //each value exists twice to create pair-wise cards
+    let cardValues = ['bus', 'car', 'motorcycle', 'shuttle_van', 'taxi', 'truck', 'truck-monster', 'truck-pickup',
+                      'bus', 'car', 'motorcycle', 'shuttle_van', 'taxi', 'truck', 'truck-monster', 'truck-pickup'];
+    cardValues = shuffle(cardValues); //contains the values for each card
+
+    //prepare the cards:
+    // 1. add card values
+    // 2. add an event listener for each card
     for (let i = 0; i < cards.length; ++i) {
-        console.log(cards[i].innerHTML);
+        //add the card value
+        cards[i].innerHTML = cardValues[i];
+
+        //add an event listener to turn the card around
         cards[i].addEventListener('click', function () {
-            console.log(cards[i].innerHTML);
             cards[i].classList.add('open')
         });
     }
 }
 
-setup()
+
+//when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function(event)
+{
+    setup();
+});
+
+//-------------------------------------------------- Helper Functions --------------------------------------------------
+
+//taken from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+function shuffle(array) {
+    var j, x, i;
+    for (i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+    }
+    return array;
+}
