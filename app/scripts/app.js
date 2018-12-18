1eabc9f2810ca01c019cca234025b6b356f4ae5b
johannes-hoetter <johannes.hoetter@smail.inf.h-brs.de>
2018-12-18 11:55:35 +0100

    Rating included

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,14 @@
 
 const cards = document.getElementsByClassName('card');
+let openedCards = [];
+let matchedCards = [];
+let rounds = 0;
 
 function setup() {
 
     //each value exists twice to create pair-wise cards
-    let cardValues = ['bus', 'car', 'motorcycle', 'shuttle_van', 'taxi', 'truck', 'truck-monster', 'truck-pickup',
-                      'bus', 'car', 'motorcycle', 'shuttle_van', 'taxi', 'truck', 'truck-monster', 'truck-pickup'];
+    let cardValues = ['bus', 'car', 'motorcycle', 'shuttle-van', 'taxi', 'truck', 'truck-monster', 'truck-pickup',
+                      'bus', 'car', 'motorcycle', 'shuttle-van', 'taxi', 'truck', 'truck-monster', 'truck-pickup'];
     cardValues = shuffle(cardValues); //contains the values for each card
 
     //prepare the cards:
@@ -13,12 +16,95 @@ function setup() {
     // 2. add an event listener for each card
     for (let i = 0; i < cards.length; ++i) {
         //add the card value
-        cards[i].innerHTML = cardValues[i];
+        cards[i].innerHTML = `<i class="icon fas fa-${cardValues[i]} fa-3x"></i>`;
 
         //add an event listener to turn the card around
         cards[i].addEventListener('click', function () {
-            cards[i].classList.add('open')
+            if (!openedCards.includes(cards[i]) && !matchedCards.includes(cards[i])) {
+                openedCards.push(cards[i]);
+                cards[i].classList.add('open');
+            }
+
+            if (openedCards.length === 2) {
+                console.log(checkMatch());
+            }
+            if (openedCards.length > 2) {
+                alert("Please don't open more than 2 cards at once");
+                for (let i = 0; i < openedCards.length; ++i) {
+                    openedCards[i].classList.remove('open');
+                }
+            }
         });
+    }
+
+    //initialize the status
+    const rating = document.getElementById('rating')
+    rating.innerHTML = `<i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>`
+}
+
+
+function checkMatch() {
+    updateStatus();
+    const [card1, card2] = openedCards;
+    let isMatch = false;
+    console.log(card1.innerHTML, card2.innerHTML);
+    if (card1.innerHTML === card2.innerHTML) {
+        isMatch = true;
+        matchedCards.push(...openedCards);
+    }
+    setTimeout(back, 500);
+    return isMatch;
+
+}
+
+function back() {
+    const [card1, card2] = openedCards;
+    card1.classList.remove('open');
+    card2.classList.remove('open');
+    openedCards = [];
+}
+
+function updateStatus() {
+    rounds += 1;
+    const num_moves = document.getElementById('num_moves');
+    num_moves.innerHTML = rounds;
+    if (rounds === 10) {
+        const rating = document.getElementById('rating');
+        rating.innerHTML = `<i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>`
+    }
+    if (rounds == 13) {
+        const rating = document.getElementById('rating');
+        rating.innerHTML = `<i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>`
+    }
+    if (rounds == 15) {
+        const rating = document.getElementById('rating');
+        rating.innerHTML = `<i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                            <i class="far fa-star"></i>`
+    }
+    if (rounds == 18) {
+        const rating = document.getElementById('rating');
+        rating.innerHTML = `<i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                            <i class="far fa-star"></i>`
+    }
+    if (rounds == 20) {
+        const rating = document.getElementById('rating');
+        rating.innerHTML = `<i class="fas fa-star-half-alt"></i>
+                            <i class="far fa-star"></i>
+                            <i class="far fa-star"></i>`
+    }
+    if (rounds == 23) {
+        const rating = document.getElementById('rating');
+        rating.innerHTML = `<i class="far fa-star"></i>
+                            <i class="far fa-star"></i>
+                            <i class="far fa-star"></i>`
     }
 }
 
@@ -28,6 +114,16 @@ document.addEventListener('DOMContentLoaded', function(event)
 {
     setup();
 });
+
+
+
+
+
+
+
+
+
+
 
 //-------------------------------------------------- Helper Functions --------------------------------------------------
 
